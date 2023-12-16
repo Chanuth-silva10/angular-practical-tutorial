5d9a16bea664fbcbdee1c17a4ab10a27abea52f3
Chanuth <it20074418@my.sliit.lk>
2023-12-16 15:47:28 +0530

    usage of ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,12 @@ export class AppComponent {
   
   postArray: Array<string> = ['post 1', 'post 2', 'post 3', 'post 4', 'post 5'];
 
-  objArray: Array<any> = []; 
+  objArray: Array<any> = [
+    {id: 1, postTitle: 'Post 1'},
+    {id: 2, postTitle: 'Post 2'},
+    {id: 3, postTitle: 'Post 3'},
+    {id: 4, postTitle: 'Post 4'},
+  ]; 
 
   constructor(){
 
