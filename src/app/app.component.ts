f5ca4fcba04b33c830b9fe8cbc9c4550ee3b8787
Chanuth <it20074418@my.sliit.lk>
2023-12-16 15:01:02 +0530

    Fetch oject array using NgFor Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,13 @@ export class AppComponent {
   
   postArray: Array<string> = ['post 1', 'post 2', 'post 3', 'post 4', 'post 5'];
 
+  objArray: Array<any> = [
+    {id: 1, postTitle: 'Post 1'},
+    {id: 2, postTitle: 'Post 2'},
+    {id: 3, postTitle: 'Post 3'},
+    {id: 4, postTitle: 'Post 4'},
+  ]; 
+
   constructor(){
     // for(let i = 0; i < this.postArray.length; i++){
     //   // console.log(this.postArray[i]);
