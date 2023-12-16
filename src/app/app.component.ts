916399073d9e6feac8ec714f54312fe2b8f256da
Chanuth <it20074418@my.sliit.lk>
2023-12-16 10:13:21 +0530

    5 - Sharing Data Between Components - parent child relationship in components(used @input decorator)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-practice';
+  parentMessage: string = 'Message coming from parent component';
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
+
 export class PostComponent implements OnInit {
 
   title: string = 'List of Post';
+  
+  @Input() fromParent!: string;
 
   constructor() {}
+  
   ngOnInit(): void {}
 }
 // ng g c post
