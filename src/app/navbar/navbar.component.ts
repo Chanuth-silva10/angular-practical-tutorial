6f51c582c44f0b768630b2d50f2e2f13def834e3
Chanuth <it20074418@my.sliit.lk>
2023-12-16 09:16:00 +0530

    2- Better Approach for Templates & Styles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  template: `<h1>Navbar Component</h1>
-              <p>Lorem ipsum doller imk</p>`,
-  styles: [
-    `
-      h1 {
-        color: red;
-        font-size: 50px;
-      }
-    `,
-  ],
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
 
-// template: '<h1> Navbar Component</h1>',
-// styles:['h1{ color: red}'],
+export class NavbarComponent {
+
+
+}
