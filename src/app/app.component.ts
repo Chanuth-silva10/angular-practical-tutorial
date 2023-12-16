0b7f547e7d44d6125b74ec6955d8d33711714617
Chanuth <it20074418@my.sliit.lk>
2023-12-16 15:26:03 +0530

    ngIf Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,7 @@ export class AppComponent {
   
   postArray: Array<string> = ['post 1', 'post 2', 'post 3', 'post 4', 'post 5'];
 
-  objArray: Array<any> = [
-    {id: 1, postTitle: 'Post 1'},
-    {id: 2, postTitle: 'Post 2'},
-    {id: 3, postTitle: 'Post 3'},
-    {id: 4, postTitle: 'Post 4'},
-  ]; 
+  objArray: Array<any> = []; 
 
   constructor(){
 
@@ -25,12 +20,6 @@ export class AppComponent {
   addNew() {
     this.objArray.push({id: 6,postTitle: 'Post 6' })
   }
-
-  // onDelete(post: any) {
-  //   let index = this.objArray.indexOf(post);
-  //   console.log(index)
-  //   this.objArray.splice(index, 1);
-  // }
 
   onDelete(index: any) {
     this.objArray.splice(index, 1);
