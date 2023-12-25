b86890037719b5dc685cea61a8b0ab975a0237e6
Chanuth <it20074418@my.sliit.lk>
2023-12-25 21:34:24 +0530

    add

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,6 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-// what is angular - pipe
-// Pipes are used to transforming data into a particular format when we only need that data transformed 'in' a template. or the HTML View.
-
-// 1. Uppercase pipe
-// 2. Lowercase pipe
-// 3. Decimal / Number pipe
-// 4. Currency pipe
-// 5. Data pipe
-// 6. JSON pipe
-// 7. Precent pipe
-// 8. Slice pipe
+// Angular services
+// Dependenct Injection
+// Data Modeling and Angular Interface
